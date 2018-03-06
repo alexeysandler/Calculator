79e47223c7d41e8a4263a3ea251ed76189fb822c
Alexey Sandler <as371x@att.com>
2018-03-06 17:50:30 +0200

    Add sum, main  classes.

---
+++ src/MainCalculator.java
@@ -0,0 +1,10 @@
+public class MainCalculator {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 3;
+
+        SumClass sumClass = new SumClass();
+        System.out.println(sumClass.sum(a,b));
+
+    }
+}
---
+++ src/SumClass.java
@@ -0,0 +1,7 @@
+public class SumClass  implements Sum{
+
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
