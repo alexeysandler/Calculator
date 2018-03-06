c9d1939202c789b36461f16960463dacb8dcd9dd
Alexey Sandler <as371x@att.com>
2018-03-06 18:19:00 +0200

    Add div and mul on main.

--- src/MainCalculator.java
+++ src/MainCalculator.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 public class MainCalculator {
     public static void main(String[] args) {
@@ -7,12 +8,10 @@ public class MainCalculator {
 
         SumClass sumClass = new SumClass();
         System.out.println("sum = " +sumClass.sum(a,b));
-
         System.out.println("sub = "+new Class_sub().sub(a,b));
-
         System.out.println("mul = "+ new Class_Mul().mul(a,b));
-
         System.out.println("div = "+ new Class_Div().div(a,b));
+        System.out.println("sum = " +sumClass.sum(a,b)+" "+ "mul = "+ new Class_Mul().mul(a,b));
 
     }
 }
